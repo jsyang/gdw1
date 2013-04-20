46234a12e28f84322271ae38c099fce52cc2a769
jim <jsyang.ca@gmail.com>
2013-04-19 22:20:23 -0700

    draw stuff

--- Source/GDW1.hx
+++ Source/GDW1.hx
@@ -1,10 +1,53 @@
+import nme.Assets;
+import nme.display.Bitmap;
 import nme.display.Sprite;
+import nme.display.StageAlign;
+import nme.display.StageDisplayState;
+import nme.display.StageScaleMode;
+import nme.events.Event;
 import nme.Lib;
 
 // Game entry-point
 class GDW1 extends Sprite {
 
-  public static function main() {
+  private var guy : Bitmap;
+
+  // Entry point
+  public static function main()
+  {
     Lib.current.addChild(new GDW1());
   }
+
+  public function new() 
+  {
+    super();
+    initialize();
+    addSprite();
+  }
+
+  private function initialize () : Void {
+    
+    Lib.current.stage.align = StageAlign.TOP_LEFT;
+    Lib.current.stage.scaleMode = StageScaleMode.NO_SCALE;
+    
+    addSprite();
+    addEventListener (Event.ENTER_FRAME, this_onEnterFrame);
+  }
+
+  private function drawFrame() : Void
+  {
+
+  }
+
+  private function addSprite() : Void 
+  {
+    guy = new Bitmap(Assets.getBitmapData("images/pc.png"));
+    addChild(guy);
+  }
+
+  private function this_onEnterFrame (event:Event) : Void {
+    guy.x += 0.5;
+    guy.y += 0.5;
+  }
+
 }
