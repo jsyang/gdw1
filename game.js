e227ff3ab5aef8258c30b678f2f622bd4b5f6237
jim <jsyang.ca@gmail.com>
2013-04-20 18:54:36 -0700

    split entities into their own js file

--- game.js
+++ game.js
@@ -1,36 +1,3 @@
-
-var Entity = Class.extend({
-
-  init : function(params) {
-    for(var i in params) {
-      this[i] = params[i];
-    }
-    if(this.initCb) this.initCb();
-  },
-
-  gfx : function() {
-    return {
-      w     : this.w,
-      h     : this.h,
-      img   : this.img,
-      x     : this.x,
-      y     : this.y
-    };
-  }
-});
-
-var Player = Entity.extend({
-  initCb : function() { this.img = preloader.getFile('player'); },
-  w : 64,
-  h : 64
-});
-
-var Enemy1 = Entity.extend({
-  initCb : function() { this.img = preloader.getFile('enemy1'); },
-  w : 64,
-  h : 64
-});
-
 var GDW1 = Class.extend({
   
   preloads : {},
